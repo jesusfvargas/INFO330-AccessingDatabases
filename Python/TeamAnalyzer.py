8137908f8f03ad6e1f33051275845299ee600c57
Jesus Vargas <jfv97@uw.edu>
2023-05-02 17:38:18 -0700

    python complete

--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -1,34 +1,58 @@
-import sqlite3  # This is the package for all sqlite3 access in Python
-import sys      # This helps with command-line parameters
+import sqlite3
+import sys
 
-# All the "against" column suffixes:
-types = ["bug","dark","dragon","electric","fairy","fight",
-    "fire","flying","ghost","grass","ground","ice","normal",
-    "poison","psychic","rock","steel","water"]
+types = ["bug", "dark", "dragon", "electric", "fairy", "fight",
+         "fire", "flying", "ghost", "grass", "ground", "ice", "normal",
+         "poison", "psychic", "rock", "steel", "water"]
 
-# Take six parameters on the command-line
 if len(sys.argv) < 6:
     print("You must give me six Pokemon to analyze!")
     sys.exit()
 
-team = []
 for i, arg in enumerate(sys.argv):
     if i == 0:
         continue
 
-    # Analyze the pokemon whose pokedex_number is in "arg"
+    try:
+        pokedex_num = int(arg)
+    except ValueError:
+        print(f"Error: Invalid pokedex number {arg}")
+        sys.exit()
 
-    # You will need to write the SQL, extract the results, and compare
-    # Remember to look at those "against_NNN" column values; greater than 1
-    # means the Pokemon is strong against that type, and less than 1 means
-    # the Pokemon is weak against that type
+    conn = sqlite3.connect('../pokemon.sqlite')
+    cursor = conn.cursor()
+
+    name_and_types = ("SELECT p.name, pv.type1, pv.type2 FROM pokemon p JOIN pokemon_types_view AS pv ON p.name = pv.name WHERE p.id = " + arg + ";")
+    cursor.execute(name_and_types)
+    result = cursor.fetchone()
+    pokemon_name = result[0]
+    pokemon_types = (result[1], result[2])
+
+    cursor.execute("SELECT * FROM pokemon_types_battle_view WHERE type1name = ? and type2name = ?",
+              (pokemon_types[0], pokemon_types[1]))
+    against_result = cursor.fetchone()
+
+    new_against_result = against_result[2:]
+
+    against_map = {}
+    for against, nums in zip(types, new_against_result):
+        against_map[against] = nums
+
+    strengths = []
+    weaknesses = []
+    for against in against_map.keys():
+        if against_map[against] > 1:
+            strengths.append(against)
+        elif against_map[against] < 1:
+            weaknesses.append(against)
+
+    print(f"Analyzing {arg}")
+    print(f"{pokemon_name} ({pokemon_types[0]}{' ' + pokemon_types[1] if pokemon_types[1] else ''}) is strong against {strengths} but weak against {weaknesses}")
+    conn.close()
 
 answer = input("Would you like to save this team? (Y)es or (N)o: ")
 if answer.upper() == "Y" or answer.upper() == "YES":
     teamName = input("Enter the team name: ")
-
-    # Write the pokemon team to the "teams" table
     print("Saving " + teamName + " ...")
 else:
     print("Bye for now!")
-
